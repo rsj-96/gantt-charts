b0af96d2e1964d49cfad9bfe1cfb18f68ed6aff4
Rachel Johnston <rachel.johnston1996@gmail.com>
2025-11-20 15:49:29 +0000

    Update gantt.py
    - Add error handling

--- gantt.py
+++ gantt.py
@@ -31,134 +31,140 @@ st.write("If you do not have this template, speak with RJ or refer to 'Digital T
 
 file = st.file_uploader("Choose a '.xlsx' File", type = 'xlsx') # streamlit file uploader where the excel type is specified
 if file:
+    try:
     
-    #----- Customisation ---------
-    
-    og = pd.read_excel(file)
-    
-    st.subheader("Excel Preview")
-    st.write("Edit Data or Keep Original")
-    df = st.data_editor(og, num_rows="dynamic", hide_index=True)
-    
-    st.subheader("Modify Gantt Chart Parameters")
-    
-    with st.container(border=True):
-        st.write("Change Figure Size")
-        col1, col2 = st.columns([1,1])
+        #----- Customisation ---------
+        
+        og = pd.read_excel(file)
+        
+        st.subheader("Excel Preview")
+        st.write("Edit Data or Keep Original")
+        df = st.data_editor(og, num_rows="dynamic", hide_index=True)
+        
+        st.subheader("Modify Gantt Chart Parameters")
+        
+        with st.container(border=True):
+            st.write("Change Figure Size")
+            col1, col2 = st.columns([1,1])
+                    
+            with col1:
+                width = st.number_input("Input Width:", max_value=100, min_value=0, value=16)  #figsize=(16,6)- Default size
+            with col2:
+                height = st.number_input("Input Height:", max_value=100, min_value=0, value=6)
                 
-        with col1:
-            width = st.number_input("Input Width:", max_value=100, min_value=0, value=16)  #figsize=(16,6)- Default size
-        with col2:
-            height = st.number_input("Input Height:", max_value=100, min_value=0, value=6)
-            
-        with col1:
-            size = st.number_input("Input Font Size:", max_value=100, min_value=0, value=15)
-    
-    with st.container(border=True):
-        st.write("Update Colour Scheme (if required)")
+            with col1:
+                size = st.number_input("Input Font Size:", max_value=100, min_value=0, value=15)
+        
+        with st.container(border=True):
+            st.write("Update Colour Scheme (if required)")
+                    
+            variables = []
+            df['group'] = np.floor(df['Group']) 
                 
-        variables = []
-        df['group'] = np.floor(df['Group']) 
+            num_variables = st.number_input("Number of Tasks", min_value=1, max_value=20, value=(df["group"].nunique()), step=1)
+            default_colours = ['#F991B4', '#FFB379', '#0EC3EB', '#6CF2EC', '#005F78',  '#00ADB2', '#BABAFF', "#8080ff",]
+            colour_dict = {}
             
-        num_variables = st.number_input("Number of Tasks", min_value=1, max_value=20, value=(df["group"].nunique()), step=1)
-        default_colours = ['#F991B4', '#FFB379', '#0EC3EB', '#6CF2EC', '#005F78',  '#00ADB2', '#BABAFF', "#8080ff",]
-        colour_dict = {}
-        
-        for i in range(0, num_variables, 3):
-            cols = st.columns(3)
-            for j in range(3):
-                idx = i + j
-                if idx < num_variables:
-                    default = default_colours[idx % len(default_colours)]
-                    picked = cols[j].color_picker(f'Group {idx+1}', default)
-                    colour_dict[idx+1] = picked
-        
-
-        
-    st.subheader("Gantt Chart")
+            for i in range(0, num_variables, 3):
+                cols = st.columns(3)
+                for j in range(3):
+                    idx = i + j
+                    if idx < num_variables:
+                        default = default_colours[idx % len(default_colours)]
+                        picked = cols[j].color_picker(f'Group {idx+1}', default)
+                        colour_dict[idx+1] = picked
+            
     
-    if st.button("Generate Gantt Chart"):
-        #---- File Processing -----
-        
-        df = df.dropna(how="all") # drop any acidental empty rows
-        df = df.sort_values(by ="Group", ascending=False) # sort data frame so Gantt chart is presented in the right way
-        df["Start_Date"] = pd.to_datetime(df["Start_Date"], format="%d-%m-%Y")
-
-        #----Ordering dataframe----
-        # project start date
-        proj_start = df.Start_Date.min()
-
-        # caluculating actual end dates from number of days (including the weekend)
-        df["Planned_End"] = np.busday_offset(df['Start_Date'].values.astype('datetime64[D]'), df['FTE_Days'] - 1, roll='forward', weekmask='1111100')
-
-        df["Completed_end"] = np.where(df["Completed_FTE_Days"] > 0, np.busday_offset( df["Start_Date"].values.astype("datetime64[D]"), df["Completed_FTE_Days"] - 1, roll="forward", weekmask="1111100"),
-        df["Start_Date"]) 
-
-        # Calculating actual days
-
-        df["Planned_days"] = (df["Planned_End"] - df["Start_Date"]).dt.days + 1
-
-        df["Completed_days"] = (df["Completed_end"] - df["Start_Date"]).dt.days + 1
-        
-        df.loc[df["Completed_FTE_Days"] <= 0, "Completed_days"] = 0
-
-        # days between start and current progression of each task
-        df['current_num'] = (df.Planned_days * df.Completed_days)
-
-
-        #------ Colours for the Gantt Chart--------
-        df['group'] = np.floor(df['Group'])
-
-        def color(row):
-            #color_dict = {1.0 :'#F991B4', 2.0:'#FFB379', 3.0:'#0EC3EB', 4.0:'#6CF2EC', 5.0:'#005F78', 6.0 : '#00ADB2', 7.0 : '#BABAFF', 8.0: "#8080ff", 9.0: "#FFB379"}
-            return colour_dict[row['group']]
-
-        df['color'] = df.apply(color, axis=1)
-
-        
-        #st.dataframe(df) #check df
-        
-        #-------Plotting---------
-
-        fig, ax = plt.subplots(1, figsize=(width,height))
-
-        # bars
-        ax.barh(df.Task_Name, df.Completed_days, left=df.Start_Date, color=df.color)
-        ax.barh(df.Task_Name, df.Planned_days, left=df.Start_Date, color=df.color, alpha=0.5)
-        
-        #plotting milestone
-        if df["Milestone"].any():
-            ax.scatter( df.Milestone, df.Task_Name, facecolors="none", edgecolors="#394042", marker="D", s=150, linewidth=1.5 )
-        else: 
-            pass
-        
-        #Labelling
-        ax.xaxis.tick_top()
-        ax.xaxis.set_label_position('top')
-
-        start_date = df["Start_Date"].min()
-        end_date = df["Planned_End"].max()
-
-        month_starts = pd.date_range(start=start_date, end=end_date, freq='MS')
-        tick_positions = [start_date] + [m for m in month_starts if m > start_date]
-        tick_labels = [start_date.strftime("%b %Y")] + [m.strftime("%b %Y") for m in month_starts if m > start_date]
-        ax.set_xlim(start_date, end_date)
-        ax.set_xticks(tick_positions)
-        ax.set_xticklabels(tick_labels)
-        ax.tick_params(axis="both", labelsize=size)
-
-        st.pyplot(fig)
-        
-        gantt_fig = io.BytesIO()
             
-        fig.savefig(gantt_fig, format="png", dpi=600, bbox_inches="tight")
+        st.subheader("Gantt Chart")
+        
+        if st.button("Generate Gantt Chart"):
+            #---- File Processing -----
             
-        st.download_button(
-            label="Download Image",
-            data=gantt_fig.getvalue(),
-            file_name="Gantt_chart.png",
-            mime="image/png"
-            )
+            df = df.dropna(how="all") # drop any acidental empty rows
+            df = df.sort_values(by ="Group", ascending=False) # sort data frame so Gantt chart is presented in the right way
+            df["Start_Date"] = pd.to_datetime(df["Start_Date"], format="%d-%m-%Y")
+    
+            #----Ordering dataframe----
+            # project start date
+            proj_start = df.Start_Date.min()
+    
+            # caluculating actual end dates from number of days (including the weekend)
+            df["Planned_End"] = np.busday_offset(df['Start_Date'].values.astype('datetime64[D]'), df['FTE_Days'] - 1, roll='forward', weekmask='1111100')
+    
+            df["Completed_end"] = np.where(df["Completed_FTE_Days"] > 0, np.busday_offset( df["Start_Date"].values.astype("datetime64[D]"), df["Completed_FTE_Days"] - 1, roll="forward", weekmask="1111100"),
+            df["Start_Date"]) 
+    
+            # Calculating actual days
+    
+            df["Planned_days"] = (df["Planned_End"] - df["Start_Date"]).dt.days + 1
+    
+            df["Completed_days"] = (df["Completed_end"] - df["Start_Date"]).dt.days + 1
+            
+            df.loc[df["Completed_FTE_Days"] <= 0, "Completed_days"] = 0
+    
+            # days between start and current progression of each task
+            df['current_num'] = (df.Planned_days * df.Completed_days)
+    
+    
+            #------ Colours for the Gantt Chart--------
+            df['group'] = np.floor(df['Group'])
+    
+            def color(row):
+                #color_dict = {1.0 :'#F991B4', 2.0:'#FFB379', 3.0:'#0EC3EB', 4.0:'#6CF2EC', 5.0:'#005F78', 6.0 : '#00ADB2', 7.0 : '#BABAFF', 8.0: "#8080ff", 9.0: "#FFB379"}
+                return colour_dict[row['group']]
+    
+            df['color'] = df.apply(color, axis=1)
+    
+            
+            #st.dataframe(df) #check df
+            
+            #-------Plotting---------
+    
+            fig, ax = plt.subplots(1, figsize=(width,height))
+    
+            # bars
+            ax.barh(df.Task_Name, df.Completed_days, left=df.Start_Date, color=df.color)
+            ax.barh(df.Task_Name, df.Planned_days, left=df.Start_Date, color=df.color, alpha=0.5)
+            
+            #plotting milestone
+            if df["Milestone"].any():
+                ax.scatter( df.Milestone, df.Task_Name, facecolors="none", edgecolors="#394042", marker="D", s=150, linewidth=1.5 )
+            else: 
+                pass
+            
+            #Labelling
+            ax.xaxis.tick_top()
+            ax.xaxis.set_label_position('top')
+    
+            start_date = df["Start_Date"].min()
+            end_date = df["Planned_End"].max()
+    
+            month_starts = pd.date_range(start=start_date, end=end_date, freq='MS')
+            tick_positions = [start_date] + [m for m in month_starts if m > start_date]
+            tick_labels = [start_date.strftime("%b %Y")] + [m.strftime("%b %Y") for m in month_starts if m > start_date]
+            ax.set_xlim(start_date, end_date)
+            ax.set_xticks(tick_positions)
+            ax.set_xticklabels(tick_labels)
+            ax.tick_params(axis="both", labelsize=size)
+    
+            st.pyplot(fig)
+            
+            gantt_fig = io.BytesIO()
+                
+            fig.savefig(gantt_fig, format="png", dpi=600, bbox_inches="tight")
+                
+            st.download_button(
+                label="Download Image",
+                data=gantt_fig.getvalue(),
+                file_name="Gantt_chart.png",
+                mime="image/png"
+                )
+    
+    except Exception as e:
+            st.error(f"An error occurred: {e}")
+            st.stop()
+
 
 
 
